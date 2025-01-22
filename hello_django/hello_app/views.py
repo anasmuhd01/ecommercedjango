6e1d6934ef067dcdf53c731f1431fba7f211d9ae
anasmuhd01 <anasmuhd010@gmail.com>
2025-01-22 18:14:13 +0000

    html templates

--- hello_django/hello_app/views.py
+++ hello_django/hello_app/views.py
@@ -3,4 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 def printHelo(request):
-    return render(request,'hello.html')
+    movie_list ={
+        'movies':[
+            {
+        'title':'Godfather',
+        'year':'1999',
+        'summary':'a nice movie',
+        'success': True
+
+    },{
+        'title':'Hector solomonka',
+        'year':'2010',
+        'summary':'a  movie about solomonka',
+        'success': False
+        
+    },{
+        'title':'willian arc',
+        'year':'2020',
+        'summary':'a villian movie',
+        'success': True
+        
+    }
+    ]
+    }
+    return render(request,'hello.html',movie_list)
