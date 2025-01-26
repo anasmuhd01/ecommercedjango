3a5057039236159e9e2aab8a5d426e92fc8d3069
anasmuhd01 <anasmuhd010@gmail.com>
2025-01-26 20:31:59 +0000

    images for template and input form

--- movie_manager/movie_manager/settings.py
+++ movie_manager/movie_manager/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -118,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -3,10 +3,38 @@ from django.http import HttpResponse
 # Create your views here.
 
 def create(request):
+    if request.POST:
+        print(request.POST).get('title')
     return render(request,'create.html')
 
 def list(request):
-    return render(request, 'list.html')
+    movie_list ={
+        'movies':[
+            {
+        'title':'Godfather',
+        'year':'1999',
+        'summary':'a nice movie',
+        'img':'Logo.png',
+        'success': True
+
+    },{
+        'title':'Hector solomonka',
+        'year':'2010',
+        'summary':'a  movie about solomonka',
+        'img':'Logo.png',
+        'success': False
+        
+    },{
+        'title':'willian arc',
+        'year':'2020',
+        'summary':'a villian movie',
+        'img':'Logo.png',
+        'success': True
+        
+    }
+    ]
+    }
+    return render(request, 'list.html',movie_list)
 
 def edit(request):
     return render(request, 'edit.html')
